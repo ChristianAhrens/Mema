21112c2fe64df0f4fccb8040d8e3f8c22dd8b930
Christian Ahrens <christianahrens@me.com>
2025-01-03 21:57:35 +0100

    Add juce::PluginListComponent usage

--- Source/MemaEditor/PluginControlComponent.cpp
+++ Source/MemaEditor/PluginControlComponent.cpp
@@ -36,27 +36,16 @@ PluginControlComponent::PluginControlComponent()
 	addAndMakeVisible(m_spacing1.get());
 
 	m_showEditorButton = std::make_unique<juce::TextButton>("None", "Show plug-in editor");
-	m_showEditorButton->onClick = [this] {};
+	m_showEditorButton->onClick = [this] { if (onShowPluginEditor) onShowPluginEditor(); };
 	addAndMakeVisible(m_showEditorButton.get());
 
 	m_triggerSelectButton = std::make_unique<juce::DrawableButton>("Show plug-in selection menu", juce::DrawableButton::ButtonStyle::ImageOnButtonBackground);
 	m_triggerSelectButton->setTooltip("Show plug-in selection menu");
-	m_triggerSelectButton->onClick = [this] {
-		juce::PopupMenu menu;
-		menu.addItem(1, "Dummy1");
-		menu.addItem(2, "Dummy2");
-		menu.addItem(3, "Dummy3");
-		menu.showMenuAsync(juce::PopupMenu::Options(), [this](int idx) { DBG(juce::String(__FUNCTION__) << " " << idx); });
-	};
+	m_triggerSelectButton->onClick = [this] { showPluginsList(juce::Desktop::getMousePosition()); };
 	addAndMakeVisible(m_triggerSelectButton.get());
 
-	m_spacing2 = std::make_unique<Spacing>();
-	addAndMakeVisible(m_spacing2.get());
-
-	m_rescanButton = std::make_unique<juce::DrawableButton>("Re-scan plug-ins", juce::DrawableButton::ButtonStyle::ImageOnButtonBackground);
-	m_rescanButton->setTooltip("Re-scan plug-ins");
-	m_rescanButton->onClick = [this] { scanPlugins(); };
-	addAndMakeVisible(m_rescanButton.get());
+	m_pluginSelectionComponent = std::make_unique<PluginListAndSelectComponent>();
+	m_pluginSelectionComponent->onPluginSelected = [=](const juce::PluginDescription& pluginDescription) { if (onPluginSelected) onPluginSelected(pluginDescription); };
 }
 
 PluginControlComponent::~PluginControlComponent()
@@ -64,18 +53,20 @@ PluginControlComponent::~PluginControlComponent()
 
 }
 
-void PluginControlComponent::scanPlugins()
+void PluginControlComponent::showPluginsList(juce::Point<int> showPosition)
 {
-	juce::AudioPluginFormatManager formatManager;
-	formatManager.addDefaultFormats();
-	juce::KnownPluginList pluginList;
-	juce::File deadMansPedalFile;
-	juce::PropertiesFile::Options options;
-	juce::PropertiesFile propertiesFile(options);
-	juce::PluginListComponent pluginListComponent(formatManager, pluginList, deadMansPedalFile, &propertiesFile);
-	juce::FileSearchPath directoryPaths;
-	juce::PluginDirectoryScanner scanner(pluginList, *formatManager.getFormat(0), directoryPaths, true, deadMansPedalFile);
+	m_pluginSelectionComponent->setVisible(true);
+	m_pluginSelectionComponent->addToDesktop(juce::ComponentPeer::windowHasDropShadow);
 
+	auto const display = juce::Desktop::getInstance().getDisplays().getPrimaryDisplay();
+	if (nullptr != display && nullptr != m_pluginSelectionComponent)
+	{
+		if (display->totalArea.getHeight() < showPosition.getY() + m_pluginSelectionComponent->getHeight())
+			showPosition.setY(showPosition.getY() - m_pluginSelectionComponent->getHeight() - 30);
+		if (display->totalArea.getWidth() < showPosition.getX() + m_pluginSelectionComponent->getWidth())
+			showPosition.setX(showPosition.getX() - m_pluginSelectionComponent->getWidth() - 30);
+	}
+	m_pluginSelectionComponent->setTopLeftPosition(showPosition);
 }
 
 void PluginControlComponent::resized()
@@ -87,10 +78,6 @@ void PluginControlComponent::resized()
 		m_enableButton->setBounds(bounds.removeFromLeft(bounds.getHeight()));
 	if (m_spacing1)
 		m_spacing1->setBounds(bounds.removeFromLeft(margin));
-	if (m_rescanButton)
-		m_rescanButton->setBounds(bounds.removeFromRight(bounds.getHeight()));
-	if (m_spacing2)
-		m_spacing2->setBounds(bounds.removeFromRight(margin));
 	if (m_triggerSelectButton)
 		m_triggerSelectButton->setBounds(bounds.removeFromRight(bounds.getHeight()));
 	if (m_showEditorButton)
@@ -112,10 +99,6 @@ void PluginControlComponent::lookAndFeelChanged()
 	auto triggerSelectDrawable = juce::Drawable::createFromSVG(*juce::XmlDocument::parse(BinaryData::stat_minus_1_24dp_svg).get());
 	triggerSelectDrawable->replaceColour(juce::Colours::black, getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
 	m_triggerSelectButton->setImages(triggerSelectDrawable.get());
-
-	auto rescanButtonDrawable = juce::Drawable::createFromSVG(*juce::XmlDocument::parse(BinaryData::replay_24dp_svg).get());
-	rescanButtonDrawable->replaceColour(juce::Colours::black, getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
-	m_rescanButton->setImages(rescanButtonDrawable.get());
 
 	juce::Component::lookAndFeelChanged();
 }
--- Source/MemaEditor/PluginControlComponent.h
+++ Source/MemaEditor/PluginControlComponent.h
@@ -25,7 +25,7 @@ namespace Mema
 {
 
 //==============================================================================
-class Spacing :public juce::Component
+class Spacing : public juce::Component
 {
 public:
     Spacing() = default;
@@ -39,13 +39,77 @@ public:
 };
 
 //==============================================================================
+class PluginListAndSelectComponent : public juce::Component
+{
+public:
+    PluginListAndSelectComponent()
+    {
+        m_formatManager.addDefaultFormats();
+        juce::File deadMansPedalFile;
+        juce::PropertiesFile::Options options;
+        juce::PropertiesFile propertiesFile(options);
+        m_pluginListComponent = std::make_unique<juce::PluginListComponent>(m_formatManager, m_pluginList, deadMansPedalFile, &propertiesFile);
+        m_pluginListComponent->getTableListBox().setMultipleSelectionEnabled(false);
+        addAndMakeVisible(m_pluginListComponent.get());
+
+        m_selectButton = std::make_unique<juce::TextButton>("Select", "Accept the current plugin selection as new type to instantiate and close.");
+        m_selectButton->onClick = [=]() {
+            if (m_pluginListComponent)
+            {
+                jassert(1 == m_pluginListComponent->getTableListBox().getSelectedRows().size());
+            }
+            removeFromDesktop();
+        };
+        m_selectButton->setEnabled(false);
+        addAndMakeVisible(m_selectButton.get());
+
+        m_cancelButton = std::make_unique<juce::TextButton>("Cancel", "Discard the current plugin selection and close.");
+        m_cancelButton->onClick = [=]() {
+            removeFromDesktop();
+        };
+        addAndMakeVisible(m_cancelButton.get());
+
+        setSize(885, 600);
+    };
+    ~PluginListAndSelectComponent() = default;
+
+    //==============================================================================
+    void paint(Graphics& g) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    };
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        m_pluginListComponent->setBounds(bounds);
+
+        bounds = bounds.removeFromBottom(28);
+        m_cancelButton->setBounds(bounds.removeFromRight(80).reduced(2));
+        m_selectButton->setBounds(bounds.removeFromRight(80).reduced(2));
+    };
+
+    //==============================================================================
+    std::function<void(const juce::PluginDescription&)> onPluginSelected;
+
+private:
+    //==============================================================================
+    juce::AudioPluginFormatManager              m_formatManager;
+    juce::KnownPluginList                       m_pluginList;
+
+    std::unique_ptr<juce::PluginListComponent>  m_pluginListComponent;
+    std::unique_ptr<juce::TextButton>           m_selectButton;
+    std::unique_ptr<juce::TextButton>           m_cancelButton;
+};
+
+//==============================================================================
 class PluginControlComponent :   public juce::Component
 {
 public:
     PluginControlComponent();
     ~PluginControlComponent();
 
-    void scanPlugins();
+    void showPluginsList(juce::Point<int> showPosition);
 
     //==============================================================================
     void paint (Graphics&) override;
@@ -54,6 +118,7 @@ public:
 
     //==============================================================================
     std::function<void(const juce::PluginDescription&)> onPluginSelected;
+    std::function<void()> onShowPluginEditor;
     std::function<void(bool)> onPluginEnabledChange;
 
 private:
@@ -62,9 +127,10 @@ private:
     std::unique_ptr<Spacing>                m_spacing1;
     std::unique_ptr<juce::TextButton>       m_showEditorButton;
     std::unique_ptr<juce::DrawableButton>   m_triggerSelectButton;
-    std::unique_ptr<Spacing>                m_spacing2;
-    std::unique_ptr<juce::DrawableButton>   m_rescanButton;
-    
+
+    std::unique_ptr<PluginListAndSelectComponent>  m_pluginSelectionComponent;
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginControlComponent)
 };
 
