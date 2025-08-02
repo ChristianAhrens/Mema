48d04cd2a4ecb8b013e9cce6451c25edca3e6904
Christian Ahrens <christianahrens@me.com>
2025-08-02 12:31:08 +0200

    Fixed MemaClientDiscoverComponent to allow multiple Mema clients on one UNIX host
    ...by adding code clode of juce::NetworkServiceDiscovery::AvailableServiceList "PortReuseAvailableServiceList" that simply adds a socket.setEnablePortReuse(true)

--- Source/MemaClientCommon/MemaClientDiscoverComponent.cpp
+++ Source/MemaClientCommon/MemaClientDiscoverComponent.cpp
@@ -20,6 +20,135 @@
 
 #include <MemaProcessor/MemaServiceData.h>
 
+
+
+ //==============================================================================
+PortReuseAvailableServiceList::PortReuseAvailableServiceList(const juce::String& serviceType, int broadcastPort)
+    : juce::Thread(SystemStats::getJUCEVersion() + ": Discovery_listen"), serviceTypeUID(serviceType)
+{
+#if JUCE_ANDROID
+    acquireMulticastLock();
+#endif
+
+    socket.setEnablePortReuse(true);
+    socket.bindToPort(broadcastPort);
+    startThread(juce::Thread::Priority::background);
+}
+
+PortReuseAvailableServiceList::~PortReuseAvailableServiceList()
+{
+    socket.shutdown();
+    stopThread(2000);
+
+#if JUCE_ANDROID
+    releaseMulticastLock();
+#endif
+}
+
+void PortReuseAvailableServiceList::run()
+{
+    while (!threadShouldExit())
+    {
+        if (socket.waitUntilReady(true, 200) == 1)
+        {
+            char buffer[1024];
+            auto bytesRead = socket.read(buffer, sizeof(buffer) - 1, false);
+
+            if (bytesRead > 10)
+                if (auto xml = parseXML(juce::String(juce::CharPointer_UTF8(buffer),
+                    juce::CharPointer_UTF8(buffer + bytesRead))))
+                    if (xml->hasTagName(serviceTypeUID))
+                        handleMessage(*xml);
+        }
+
+        removeTimedOutServices();
+    }
+}
+
+std::vector<juce::NetworkServiceDiscovery::Service> PortReuseAvailableServiceList::getServices() const
+{
+    const juce::ScopedLock sl(listLock);
+    auto listCopy = services;
+    return listCopy;
+}
+
+void PortReuseAvailableServiceList::handleAsyncUpdate()
+{
+    juce::NullCheckedInvocation::invoke(onChange);
+}
+
+void PortReuseAvailableServiceList::handleMessage(const XmlElement& xml)
+{
+    juce::NetworkServiceDiscovery::Service service;
+    service.instanceID = xml.getStringAttribute("id");
+
+    if (service.instanceID.trim().isNotEmpty())
+    {
+        service.description = xml.getStringAttribute("name");
+        service.address = juce::IPAddress(xml.getStringAttribute("address"));
+        service.port = xml.getIntAttribute("port");
+        service.lastSeen = juce::Time::getCurrentTime();
+
+        handleMessage(service);
+    }
+}
+
+static void sortServiceList(std::vector<juce::NetworkServiceDiscovery::Service>& services)
+{
+    auto compareServices = [](const juce::NetworkServiceDiscovery::Service& s1,
+        const juce::NetworkServiceDiscovery::Service& s2)
+        {
+            return s1.instanceID < s2.instanceID;
+        };
+
+    std::sort(services.begin(), services.end(), compareServices);
+}
+
+void PortReuseAvailableServiceList::handleMessage(const juce::NetworkServiceDiscovery::Service& service)
+{
+    const juce::ScopedLock sl(listLock);
+
+    for (auto& s : services)
+    {
+        if (s.instanceID == service.instanceID)
+        {
+            if (s.description != service.description
+                || s.address != service.address
+                || s.port != service.port)
+            {
+                s = service;
+                triggerAsyncUpdate();
+            }
+
+            s.lastSeen = service.lastSeen;
+            return;
+        }
+    }
+
+    services.push_back(service);
+    sortServiceList(services);
+    triggerAsyncUpdate();
+}
+
+void PortReuseAvailableServiceList::removeTimedOutServices()
+{
+    const double timeoutSeconds = 5.0;
+    auto oldestAllowedTime = juce::Time::getCurrentTime() - juce::RelativeTime::seconds(timeoutSeconds);
+
+    const juce::ScopedLock sl(listLock);
+
+    auto oldEnd = std::end(services);
+    auto newEnd = std::remove_if(std::begin(services), oldEnd,
+        [=](const juce::NetworkServiceDiscovery::Service& s) { return s.lastSeen < oldestAllowedTime; });
+
+    if (newEnd != oldEnd)
+    {
+        services.erase(newEnd, oldEnd);
+        triggerAsyncUpdate();
+    }
+}
+
+
 MemaClientDiscoverComponent::MemaClientDiscoverComponent()
     : juce::Component()
 {
@@ -86,10 +215,11 @@ void MemaClientDiscoverComponent::setupServiceDiscovery()
     // scope to autodestruct testSocket
     {
         auto testSocket = std::make_unique<juce::DatagramSocket>();
+        testSocket->setEnablePortReuse(true);
         if (!testSocket->bindToPort(Mema::ServiceData::getBroadcastPort()))
         {
             auto conflictTitle = "Service discovery error";
-            auto conflictInfo = "Unable to discover Mema instances\nas the discovery broadcast port is already in use.\n\nMaybe another Mema client\nis running on this host?";
+            auto conflictInfo = "Unable to discover Mema instances\nas the discovery broadcast port cannot be used.";
             juce::AlertWindow::showOkCancelBox(juce::MessageBoxIconType::WarningIcon, conflictTitle, conflictInfo, "Retry", "Quit", nullptr, juce::ModalCallbackFunction::create([this](int result) {
                 if (1 == result)
                     setupServiceDiscovery();
@@ -99,7 +229,7 @@ void MemaClientDiscoverComponent::setupServiceDiscovery()
         }
     }
 
-    m_availableServices = std::make_unique<juce::NetworkServiceDiscovery::AvailableServiceList>(
+    m_availableServices = std::make_unique<PortReuseAvailableServiceList>(
         Mema::ServiceData::getServiceTypeUID(),
         Mema::ServiceData::getBroadcastPort());
 
