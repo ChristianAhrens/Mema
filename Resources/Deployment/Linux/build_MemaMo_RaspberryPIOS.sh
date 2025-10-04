6ef495f85d0a909fe162080e70ed8c44fe33b7b5
Christian Ahrens <christianahrens@me.com>
2025-10-04 21:00:25 +0200

    Added Linux(ARM) Raspberry Pi release deployment automation
    commit eabda87e8dfda3086d6f9043d0cba855ccd92f8c
    Author: Christian Ahrens <christianahrens@me.com>
    Date:   Sat Oct 4 20:50:28 2025 +0200
    
        Fixed intermediate collection folder creation
    
    commit 29cda3ed0ea80333d3fd6c12b45e398fce89f9c9
    Author: Christian Ahrens <christianahrens@me.com>
    Date:   Sat Oct 4 20:16:26 2025 +0200
    
        Fixed ambigous deployment output by introducing intermediate collection folder
    
    commit 03fb6282d760ded87e3a992b77d0754233f84cdc
    Author: Christian Ahrens <christianahrens@me.com>
    Date:   Sat Oct 4 14:21:26 2025 +0200
    
        Improved zip contents bundling and cleanup
    
    commit b10f61e77eb8af3a8ae2fc1cab1ccced04c36482
    Author: Christian Ahrens <christianahrens@me.com>
    Date:   Sat Oct 4 14:11:20 2025 +0200
    
        Fixed zip bundling
    
    commit 9c50beb0401556f442d0bedff47e629847399681
    Author: Christian Ahrens <christianahrens@me.com>
    Date:   Sat Oct 4 13:52:12 2025 +0200
    
        Improved Linux / RasperryPi deployment scripts

--- Resources/Deployment/Linux/build_MemaMo_RaspberryPIOS.sh
+++ Resources/Deployment/Linux/build_MemaMo_RaspberryPIOS.sh
@@ -1,24 +1,6 @@
 # get required dependencies on Raspberry PI OS
-yes | sudo apt-get update
-yes | sudo apt-get upgrade
-yes | sudo apt install	clang \
-						g++ \
-						libasound2-dev \
-						libjack-jackd2-dev \
-						ladspa-sdk \
-						libcurl4-openssl-dev \
-						libfreetype6-dev \
-						libfontconfig1-dev \
-						libx11-dev \
-						libxcomposite-dev \
-						libxcursor-dev \
-						libxext-dev \
-						libxinerama-dev \
-						libxrandr-dev \
-						libxrender-dev \
-						libwebkit2gtk-4.0-dev \
-						libglu1-mesa-dev \
-						mesa-common-dev
+chmod +x get_dependencies_RaspberryPIOS.sh
+./get_dependencies_RaspberryPIOS.sh
 
 # we are in Resources/Deployment/Linux/ -> change directory to project root
 cd ../../../
@@ -29,9 +11,14 @@ git submodule update --init --recursive
 # we are in project root, change to buildscript directory
 cd Resources/Deployment/Linux
 
-# start the buildscript (make -j 2 instead of default 8 to not overload the poor raspi)
+# start the buildscript (make -j n instead of default 8 to not overload the poor raspi)
+Cores=2
+if ! [ -z "$1" ]
+then
+  Cores="$1"
+fi
 chmod +x build_MemaMo.sh
-./build_MemaMo.sh 2
+./build_MemaMo.sh $Cores
 
 # -> after a successful build, one could e.g. use the following as contents for .xsession to have the app start in kind of a kiosk mode. VNC of raspbian will still work in this scenario btw
 ##!/bin/sh
--- Resources/Deployment/Linux/create_installers.sh
+++
@@ -1,8 +0,0 @@
-chmod +rwx build_Mema.sh
-./build_Mema.sh
-
-chmod +rwx build_MemaMo.sh
-./build_MemaMo.sh
-
-chmod +rwx build_MemaRe.sh
-./build_MemaRe.sh
