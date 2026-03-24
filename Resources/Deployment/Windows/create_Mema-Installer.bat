348a0e917198c80e605a77ef931623400ca713ce
Christian Ahrens <christianahrens@me.com>
2026-03-24 21:08:01 +0100

    Added VS2022 support back to windows deployment infrastructure and make use of in AppVeyor CI build definitions

--- Resources/Deployment/Windows/create_Mema-Installer.bat
+++ Resources/Deployment/Windows/create_Mema-Installer.bat
@@ -2,18 +2,29 @@
 
 set INNOSETUPDIR=C:\Program Files (x86)\Inno Setup 6
 set WORKSPACE=..\..\..
-echo.
 
+set VSVER=%1
+if "%VSVER%"=="" set VSVER=VS2026
+
+if "%VSVER%"=="VS2022" (
+    set ISSFILE=create_Mema-installer_VS2022.iss
+) else (
+    set ISSFILE=create_Mema-installer.iss
+)
+
+echo.
 echo Using variables:
 echo INNOSETUPDIR = %INNOSETUPDIR%
-echo WORKSPACE = %WORKSPACE%
+echo WORKSPACE    = %WORKSPACE%
+echo VSVER        = %VSVER%
+echo ISSFILE      = %ISSFILE%
 echo.
 
 echo Build project
-CALL build_Mema.bat
+CALL build_Mema.bat %VSVER%
 
 echo Build installer
-"%INNOSETUPDIR%\ISCC.exe" create_Mema-installer.iss
+"%INNOSETUPDIR%\ISCC.exe" %ISSFILE%
 echo.
 
 echo Move setup executable to workspace root
